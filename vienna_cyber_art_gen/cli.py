a390b1cac46f90f2ee716f16d72e1eb4379affd9
Jason Walsh <j@wal.sh>
2023-10-23 13:00:16 +0100

    Save images

--- vienna_cyber_art_gen/cli.py
+++ vienna_cyber_art_gen/cli.py
@@ -13,7 +13,7 @@ def cli():
 )
 @click.option(
     "--output-dir",
-    default="generated_images",
+    default="images",
     help="Output directory for generated images",
 )
 def main(api_key, output_dir):
@@ -29,29 +29,56 @@ def main(api_key, output_dir):
     generator = PromptGenerator(api_key)
     prompts = [
         {
-            "filename": "cyberpunk_vienna.jpg",
-            "prompt_text": "Imagine a cityscape that fuses the neon-lit, cyberpunk future with the opulent elegance of early 20th-century Vienna. Capture the contrast and blend between these two worlds.",
+            "filename": "exploration_of_mind",
+            "prompt": "Generate an artistic image representing the exploration of the unconscious mind through the lens of Vienna's art and science at the turn of the 20th century blended with a futuristic cyberpunk aesthetic. The image should visualize probing the boundaries of identity and consciousness.",
         },
         {
-            "filename": "artistic_dystopia.jpg",
-            "prompt_text": "Visualize an art exhibit that showcases a dystopian vision, where cybernetic enhancements and avant-garde art coexist, highlighting the juxtaposition of innovation and societal turmoil.",
+            "filename": "interplay_of_art_and_science",
+            "prompt": "Create a vivid image showcasing the intersection of art and science in understanding the human mind. Blend the art and science of Vienna 1900 with a high-tech cyberpunk world where technology and artistry shape society.",
         },
         {
-            "filename": "virtual_bohemians.jpg",
-            "prompt_text": "Create an image of a modern-day bohemian salon where artists, intellectuals, and cyberpunk enthusiasts gather in a virtual world, discussing avant-garde ideas amidst holographic displays and traditional art.",
+            "filename": "identity_and_self",
+            "prompt": "Visualize the questions of identity and self through an imaginative cyberpunk scene. Show how technological enhancements and virtual realities in a futuristic world can shift characters' self-awareness and identity. Combine with turn-of-the-century Vienna aesthetics.",
         },
         {
-            "filename": "reimagined_fashion.jpg",
-            "prompt_text": "Reimagine the fashion of Vienna in the early 20th century with a futuristic cyberpunk twist. Blend the elegance of that era with technological enhancements and urban street style.",
+            "filename": "societal_change",
+            "prompt": "Generate an image highlighting societal and cultural transformation. Contrast Vienna at the turn of the 20th century with a cyberpunk future where technological progress radically alters the social landscape. Blend the two visual styles.",
+        },
+        {
+            "filename": "dystopian_elements",
+            "prompt": "Create a haunting cyberpunk scene with dystopian elements, blended with glimpses of the turbulent early 20th century Vienna setting. Show a darkly atmospheric, oppressive vision of the future.",
+        },
+        {
+            "filename": "ethical_and_moral_questions",
+            "prompt": "Visualize the ethical dilemmas raised by exploring the mind and unchecked technological progress. Combine thought-provoking imagery from turn-of-the-century Vienna with a cyberpunk future filled with moral questions.",
+        },
+        ## Original prompts
+        {
+            "filename": "cyberpunk_vienna",
+            "prompt": "Imagine a cityscape that fuses the neon-lit, cyberpunk future with the opulent elegance of early 20th-century Vienna. Capture the contrast and blend between these two worlds.",
+        },
+        {
+            "filename": "artistic_dystopia",
+            "prompt": "Visualize an art exhibit that showcases a dystopian vision, where cybernetic enhancements and avant-garde art coexist, highlighting the juxtaposition of innovation and societal turmoil.",
+        },
+        {
+            "filename": "virtual_bohemians",
+            "prompt": "Create an image of a modern-day bohemian salon where artists, intellectuals, and cyberpunk enthusiasts gather in a virtual world, discussing avant-garde ideas amidst holographic displays and traditional art.",
+        },
+        {
+            "filename": "reimagined_fashion",
+            "prompt": "Reimagine the fashion of Vienna in the early 20th century with a futuristic cyberpunk twist. Blend the elegance of that era with technological enhancements and urban street style.",
         },
     ]
 
     for prompt_data in prompts:
         filename = prompt_data["filename"]
-        prompt_text = prompt_data["prompt_text"]
-        generator.generate_prompt(filename, prompt_text)
+        prompt_text = prompt_data["prompt"]
+        generator.generate_image(filename, prompt_text)
+        # generator.generated_texts[filename] = prompt_text
 
-    generator.save_generated_texts(output_dir)
+    generator.save_generated_images(output_dir)
+    # generator.save_generated_texts(output_dir)
 
 
 if __name__ == "__main__":
--- vienna_cyber_art_gen/generator.py
+++ vienna_cyber_art_gen/generator.py
@@ -1,68 +1,65 @@
-# vienna_cyber_art_gen/generator.py
-
-import openai
+from pathlib import Path
 from typing import Dict
 
+import openai
+import requests
+
 class PromptGenerator:
-    """
-    PromptGenerator: A class for generating images based on prompts using the OpenAI API.
 
-    Args:
-    api_key (str): Your OpenAI API key.
+  def __init__(self, api_key: str) -> None:
+    self.api_key = api_key
+    self.generated_texts: Dict[str, str] = {}
+    self.generated_images: Dict[str, str] = {}
 
-    Attributes:
-    api_key (str): The provided OpenAI API key.
-    generated_texts (Dict[str, str]): A dictionary to store generated image texts.
+  def generate_text(self, filename: str, prompt: str) -> None:
+    """Generate text using GPT-3 and store result."""
+    try:
+      response = openai.Completion.create(
+        engine="davinci",
+        prompt=prompt,
+        max_tokens=100,
+        api_key=self.api_key
+      )
+      generated_text = response.choices[0].text
+    except openai.error.OpenAIError as err:
+      print(f"Error generating text: {err}")
+      return
 
-    Methods:
-    generate_prompt(filename, prompt_text):
-    save_generated_texts(output_dir):
-    """
+    self.generated_texts[filename] = generated_text
 
-    def __init__(self, api_key: str):
-        self.api_key = api_key
-        self.generated_texts = {}
+  def generate_image(self, filename: str, prompt: str) -> None:
+    """Generate image using DALL-E and store URL."""
+    try:
+      response = openai.Image.create(
+        prompt=prompt, 
+        n=1, 
+        size="1024x1024",
+        api_key=self.api_key
+      )
+      image_url = response['data'][0]['url'] 
+    except openai.error.OpenAIError as err:
+      print(f"Error generating image: {err}")
+      return
 
-    def generate_prompt(self, filename: str, prompt_text: str):
-        """
-        Generate an image prompt based on the given text.
+    self.generated_images[filename] = image_url
 
-        Args:
-        filename (str): The filename for the generated image.
-        prompt_text (str): The text prompt to generate the image.
+  def save_generated_texts(self, output_dir: str) -> None:
+    """Save generated texts to files in output directory."""
+    Path(output_dir).mkdir(parents=True, exist_ok=True)
 
-        Raises:
-        openai.error.OpenAIError: If there is an error with the OpenAI API.
-        """
+    for filename, generated_text in self.generated_texts.items():
+      with open(Path(output_dir) / f"generated_{filename}.txt", "w") as f:
+        f.write(generated_text)
 
-        try:
-            response = openai.Completion.create(
-                engine="davinci",
-                prompt=prompt_text,
-                max_tokens=100,  # Adjust as needed
-                api_key=self.api_key
-            )
+  def save_generated_images(self, output_dir: str) -> None:
+    """Save generated images to files in output directory."""
+    Path(output_dir).mkdir(parents=True, exist_ok=True)
 
-            generated_text = response.choices[0].text
-            self.generated_texts[filename] = generated_text
-
-        except openai.error.OpenAIError as e:
-            raise e
-
-    def save_generated_texts(self, output_dir: str):
-        """
-        Save the generated image texts to files in the specified output directory.
-
-        Args:
-        output_dir (str): The directory where generated texts will be saved.
-
-        Raises:
-        IOError: If there is an issue with file operations.
-        """
-        try:
-            for filename, generated_text in self.generated_texts.items():
-                with open(f"{output_dir}/generated_{filename}.txt", "w") as file:
-                    file.write(generated_text)
-
-        except IOError as e:
-            raise e
+    for filename, image_url in self.generated_images.items():
+      response = requests.get(image_url)
+      if response.status_code == 200:
+        image_data = response.content
+        with open(Path(output_dir) / f"{filename}.png", "wb") as f:
+          f.write(image_data)
+      else:
+        print(f"Error downloading {image_url}, status {response.status_code}")
